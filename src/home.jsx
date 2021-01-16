b8f18f60b5c108a77c70ecc9b4cfd4727e91b73f
Riyaz J <j.riyazu@gmail.com>
2021-01-16 17:29:55 +0530

    Use state in home.jsx triverse the list of object using map

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-const home = () => {
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top", body: "lorem ipsum...", author: "mario", id: 3 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Home Page</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p>Written by: {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default home;
+export default Home;
