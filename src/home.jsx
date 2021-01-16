e27b99bb38896fc4d135e8471089b1430882be60
Riyaz J <j.riyazu@gmail.com>
2021-01-16 11:57:38 +0530

    FO: Use setState and Arrow function with and without params

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import { useState } from "react";
 
-const home = () => {
+const Home = () => {
+  const [name, setName] = useState("Riyaz");
+
+  const handleClick = (params) => {
+    console.log(params);
+    setName("Riyaz J");
+  };
+
+  const handleClickString = () => {
+    console.log("Button Clicked");
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <div>{name}</div>
+      <button onClick={() => handleClick(name)}>Click Me</button>
+      <button onClick={handleClickString}>Click to Console</button>
     </div>
   );
 };
 
-export default home;
+export default Home;
