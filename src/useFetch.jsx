4d43b323ae0686e6d21078d2abafb90ca52c0071
Riyaz J <j.riyazu@gmail.com>
2021-01-16 23:04:31 +0530

    Do useEffect Cleanup. It means whenever a user request for any page and change their mind and goes to different page at that time the useEffect will still be performing the fetch and as soon as the request is changed the useEffect will throw some error. So, we have to abort the fetch when the request is changed

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -13,10 +13,13 @@ const useFetch = (resource, time = 0) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    //  Use Abort Controller to cancel the fetch when the fetch is not required anymore
+    const abort = new AbortController();
+
     // Show loading message for 1 sec.
     setTimeout(() => {
-      // Fetch data from the given resource.
-      fetch(resource)
+      // Fetch data from the given resource. Add the abort signal to the fetch so that the fetch will pause.
+      fetch(resource, { signal: abort.signal })
         .then((res) => {
           if (res.ok) return res.json();
           else throw Error("Resource doesn't exists");
@@ -31,12 +34,19 @@ const useFetch = (resource, time = 0) => {
         })
         // catch the error
         .catch((err) => {
-          // Assign the error message.
-          setError(err.message);
-          // Change the status of loading.
-          setLoading(false);
+          // Check for AbortError, if the error exists just do nothing.
+          if (err.name === "AbortError") console.log("Fetch is Aborted");
+          else {
+            // Assign the error message.
+            setError(err.message);
+            // Change the status of loading.
+            setLoading(false);
+          }
         });
     }, time);
+
+    // Stop the fetch and return the abort message.
+    return () => abort.abort();
     //   Dependency variable, if the variable is changed the useEffect will trigger and fetch the data for the given resource.
   }, [resource, time]);
 
