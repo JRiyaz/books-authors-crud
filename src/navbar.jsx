abce64ea696a12423c57e1bbbd6002b71cc6f386
Riyaz J <j.riyazu@gmail.com>
2021-01-16 22:31:55 +0530

    Replace anchor (a) tag with Link tag from react-router-dom package to avoid the request to the server. Anchor tag sends the request to server. So, to avoid that use must use Link tag

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 function navbar() {
   return (
     <nav className="navbar">
       <h1>Books & Authors</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        {/* Replace anchor (a) tag with Link tag from react-router-dom. It is because the anchor tage actually
+        send request to server even if we are using router in the application. To make the request in side the 
+        React app without sending the data to the server we must use Link tag */}
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           //   style={{
           //     color: "white",
           //     backgroundColor: "#f1356d",
@@ -13,7 +17,7 @@ function navbar() {
           //   }}
         >
           New Book
-        </a>
+        </Link>
       </div>
     </nav>
   );
