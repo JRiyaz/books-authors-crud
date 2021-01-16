f157279823f7198046ab023c7198b65c9a8b3152
Riyaz J <j.riyazu@gmail.com>
2021-01-16 22:26:13 +0530

    Add router to the application by using react-router-dom package. To use router first use must install the react-router-dom package by using 'npm i react-router-dom' command.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Navbar from "./navbar";
 import Home from "./home";
+// use router
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* Swith act as Switch statment and search for requested route and trigger particular route */}
+          <Switch>
+            {/* Home route */}
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
